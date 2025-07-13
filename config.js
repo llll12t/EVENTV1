be347e2c2e3cfcde34e8e107e52e323cc3453a6d
llll12t <147978645+llll12t@users.noreply.github.com>
2025-07-13 22:20:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,9 @@ const LIFF_EVENT = '2007305761-opN68Y00'; // LIFF ID EVENT
 const LIFF_HISTORY = '2007305761-pnRkn7MM'; // LIFF ID HISTORY 
 
 // กำหนด API URL (Admin)
-const ADMIN_URL = "https://script.google.com/macros/s/AKfycbwOiSYN37QTLiFdz8F0VfVBHGGsoA7Rn6DoAQn0ZBa7Ss6E-UP6WDZVncDgnVby1cVOyQ/exec";
+//const ADMIN_URL = "https://script.google.com/macros/s/AKfycbwOiSYN37QTLiFdz8F0VfVBHGGsoA7Rn6DoAQn0ZBa7Ss6E-UP6WDZVncDgnVby1cVOyQ/exec";
+
+const ADMIN_URL = "https://script.google.com/macros/s/AKfycbzlOktoX86jaDXynV_dp6c5Eo0FfFHXlMyQ5Yfj4bRdkC1B0mmNoWizOEb26Uzf-Oo-pw/exec";
 const MEMBER_URL = 'https://script.google.com/macros/s/AKfycbyTGVFsuEyk3RPaQDun0vq-MaTMpZIfdSin13CcneDQDh_h1x7D2Vv5DkxQgvvnf6nj/exec';
 const ADDEVENT_URL = "https://script.google.com/macros/s/AKfycbwGDZurmYzm3Vw5lXB8Vl2x7ytx6WWvkpVg4699FDJOsTWPjKUg4Zs-eF2ELv0bUYUn/exec";
 
